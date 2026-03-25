f9ee84a393308e3dbd11dec215c5e791b8788888
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2026-03-25 13:40:08 +0700

    feat: update deps I guess, prettier not eslint sort

--- .prettierrc.ts
+++ .prettierrc.ts
@@ -4,8 +4,15 @@ const config: Config = {
   singleQuote: true,
   jsxSingleQuote: false,
   trailingComma: 'es5',
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
   semi: true,
-  plugins: ['prettier-plugin-package', 'prettier-plugin-tailwindcss'],
+  plugins: [
+    'prettier-plugin-package',
+    'prettier-plugin-tailwindcss',
+    '@trivago/prettier-plugin-sort-imports',
+  ],
+  importOrderParserPlugins: ['typescript', 'jsx', 'decorators'],
 };
 
 export default config;
